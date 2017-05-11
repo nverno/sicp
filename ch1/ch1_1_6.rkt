3733f621015473590147a324612cb3ff92c7393c
nverno <noah.v.peart@gmail.com>
2017-05-11 14:28:24 -0400

    squash

--- ch1/ch1_1_6.rkt
+++ ch1/ch1_1_6.rkt
@@ -1,23 +1,8 @@
-;; First Chapter exercises
+;;; First Chapter exercises
 (define (abs x)
   (if (< x 0)
       (- x)
       x))
-
-;; Return the sum of the squares of the largest two parameters
-(define (sss x1 x2 x3)
-  (let ((sorted (sort (list x1 x2 x3) >)))
-    (+ (* (car sorted) (car sorted))
-       (* (car (cdr sorted)) (car (cdr sorted))))
-    ))
-
-(sss 1 2 3)  ; => 13
-(sss 3 1 2)  ; => 13
-
-;; Compound operator => if b is negative -, else +
-(define (a-plus-abs-b a b)
-  ((if (> b 0) + -) a b))
-(a-plus-abs-b 1 -1)
 
 ;; square function
 (define (square x) (* x x))
@@ -27,6 +12,32 @@
 
 (define (f a)
   (sum-of-squares (+ a 1) (* a 2)))
+
+;;; 1.3
+;; Return the sum of the squares of the largest two parameters
+(define (sss x1 x2 x3)
+  (let ((sorted (sort (list x1 x2 x3) >)))
+    (+ (* (car sorted) (car sorted))
+       (* (car (cdr sorted)) (car (cdr sorted))))))
+
+(define (sss2 x1 x2 x3)
+  (if (> x1 x2)
+      (if (> x3 x2)
+          (sum-of-squares x1 x3)
+          (sum-of-squares x1 x2))
+      (if (> x3 x1)
+          (sum-of-squares x2 x3)
+          (sum-of-squares x1 x2))))
+
+(sss2 1 2 3)
+(sss 1 2 3)  ; => 13
+(sss2 3 1 2)
+(sss 3 1 2)  ; => 13
+
+;; Compound operator => if b is negative -, else +
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+(a-plus-abs-b 1 -1)
 
 (define (abs x)
   (cond ((> x 0) x)
