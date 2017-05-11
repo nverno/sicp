41bdbe123f24503276abc80a5044ff206263fb86
nverno <noah.v.peart@gmail.com>
2017-05-11 16:30:58 -0400

    squash

--- psets/ps1_1/ps1_1.scm
+++ psets/ps1_1/ps1_1.scm
@@ -10,7 +10,7 @@
 
 (define p3
   (lambda (a b)
-    (+ (p2 a) (p2 b))))
+    (+ (p2 a b) (p2 a b))))
 
 (define fold
   (lambda (x y)
@@ -28,6 +28,52 @@
 (define fact
   (lambda (n)
     (if (= n 0)
+        1
 	(* n (fact (- n 1))))))
 
+(define (comb n k)
+  (/ (fact n)
+     (* (fact k) (fact (- n k)))))
 
+(comb 10 2)
+
+;;; Ex 11: curried => 3
+(define foo1
+  (lambda (x)
+    (* x x)))
+
+(foo1 (foo1 (expt 3 .25)))
+
+(define foo2
+  (lambda (x y)
+    (/ x y)))
+
+(foo2 (foo2 12 2) (foo2 4 2))
+
+(define foo3
+  (lambda (x)
+    (lambda (y)
+      (/ x y))))
+
+((foo3 12) ((foo3 16) 4))
+
+(define foo4
+  (lambda (x)
+    (x 3)))
+
+(foo4 (foo4 (lambda (x) +)))
+
+(define foo5
+  (lambda (x)
+    (cond ((= x 2)
+           (lambda () x))
+          (else
+           (lambda () (* x 3))))))
+
+(+ ((foo5 ((foo5 2)))) 1)
+
+(define foo6
+  (lambda (x)
+    (x (lambda (y) (y y)))))
+
+((foo6 list) identity)
