fff744772d5e29518e44385eadd13ff592534a44
nverno <noah.v.peart@gmail.com>
2020-08-05 18:15:02 -0700

    squash

--- cs61a/hw0-1.scm
+++ cs61a/hw0-1.scm
@@ -1,3 +1,4 @@
+#lang simply-scheme
 ;; Exercise 0 - Introduce yourself
 ;; Make a followup on the "Hello World!" post on Piazza introducing yourself.
 
@@ -5,13 +6,26 @@
 ;; Exercise 1 - Define sum-of-squares
 (define (square x) (* x x))
 
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
 
 ;; Exercise 2a - Define can-drive
 
-
+(define (can-drive x)
+  (if (< x 16) '(not yet)
+      '(Good to go)))
 
 ;; Exercise 2b - Define fizzbuzz
 
+(define (divisible? x y)
+  (zero? (remainder x y)))
+
+(define (fizzbuzz x)
+  (cond
+   ((divisible? x 3)
+    (if (divisible? x 5)'fizzbuzz 'fizz))
+   ((divisible? x 5) 'buzz)
+   (#t x)))
 
 ;; Exercise 3 - Why did the Walrus cross the Serengeti?
 
